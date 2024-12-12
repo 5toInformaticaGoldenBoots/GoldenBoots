b39ccbbb31a4542195b13d75226a37705a84cef0
Heidynova <heidymelonova@gmail.com>
2024-12-12 11:52:11 -0400

    prueba
    ola

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+<<<<<<< Updated upstream:Form1.Designer.cs
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
@@ -35,5 +36,95 @@
         }
 
         #endregion
+=======
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Inicio));
+            panel1 = new Panel();
+            label1 = new Label();
+            pictureBox2 = new PictureBox();
+            pictureBox1 = new PictureBox();
+            panel2 = new Panel();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.BurlyWood;
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(pictureBox2);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(177, 749);
+            panel1.TabIndex = 0;
+            panel1.Paint += panel1_Paint;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(52, 179);
+            label1.Name = "label1";
+            label1.Size = new Size(53, 21);
+            label1.TabIndex = 2;
+            label1.Text = "Inicio";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.casa;
+            pictureBox2.Location = new Point(12, 172);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(34, 28);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(101, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(94, 44);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Silver;
+            panel2.Dock = DockStyle.Top;
+            panel2.Location = new Point(177, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(623, 98);
+            panel2.TabIndex = 1;
+            // 
+            // Inicio
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(800, 749);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            ForeColor = Color.FromArgb(0, 0, 64);
+            Name = "Inicio";
+            Text = "p";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox1;
+        private Label label1;
+>>>>>>> Stashed changes:Inicio.Designer.cs
     }
 }
