f004679450f3c280ddc9cd791fafd35b47157bf1
chrisrobles22 <chrisrobles1822@gmail.com>
2025-01-14 09:52:05 -0400

    Perfil de usuario
    perfil de usuario y ofertas

--- sobre_nosotros.Designer.cs
+++ sobre_nosotros.Designer.cs
@@ -30,24 +30,28 @@
         {
             pictureBox1 = new PictureBox();
             pictureBox2 = new PictureBox();
-            pictureBox3 = new PictureBox();
             panel1 = new Panel();
-            label1 = new Label();
             label2 = new Label();
+            label1 = new Label();
             label3 = new Label();
             label4 = new Label();
             label5 = new Label();
             label6 = new Label();
             label7 = new Label();
+            label8 = new Label();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
             panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
             SuspendLayout();
             // 
             // pictureBox1
             // 
-            pictureBox1.Location = new Point(698, 12);
+            pictureBox1.Image = GoldenBoots.Properties.Resources.compras2;
+            pictureBox1.Location = new Point(735, 0);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(43, 35);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
@@ -56,60 +60,54 @@
             // 
             // pictureBox2
             // 
-            pictureBox2.Location = new Point(747, 12);
+            pictureBox2.Image = GoldenBoots.Properties.Resources.usuario4;
+            pictureBox2.Location = new Point(784, 0);
             pictureBox2.Name = "pictureBox2";
             pictureBox2.Size = new Size(41, 35);
             pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox2.TabIndex = 2;
             pictureBox2.TabStop = false;
             // 
-            // pictureBox3
-            // 
-            pictureBox3.Location = new Point(650, 12);
-            pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(42, 35);
-            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox3.TabIndex = 3;
-            pictureBox3.TabStop = false;
-            // 
             // panel1
             // 
             panel1.BackColor = SystemColors.ActiveBorder;
             panel1.Controls.Add(label2);
             panel1.Controls.Add(label1);
-            panel1.Location = new Point(3, 53);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(pictureBox2);
+            panel1.Location = new Point(1, 0);
             panel1.Name = "panel1";
-            panel1.Size = new Size(798, 113);
+            panel1.Size = new Size(825, 113);
             panel1.TabIndex = 0;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Times New Roman", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.ForeColor = Color.Black;
-            label1.Location = new Point(280, 25);
-            label1.Name = "label1";
-            label1.Size = new Size(191, 17);
-            label1.TabIndex = 5;
-            label1.Text = "Conoce #GoldenBootsCrew";
             // 
             // label2
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Times New Roman", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label2.ForeColor = Color.Black;
-            label2.Location = new Point(128, 57);
+            label2.Location = new Point(167, 56);
             label2.Name = "label2";
             label2.Size = new Size(493, 17);
             label2.TabIndex = 6;
             label2.Text = "¡Descubre nuestro programa de lealtad y obtenga ventajas y beneficios exclusivos!";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Black;
+            label1.Location = new Point(318, 27);
+            label1.Name = "label1";
+            label1.Size = new Size(199, 17);
+            label1.TabIndex = 5;
+            label1.Text = "Conoce #GooldenBootsCrew";
             // 
             // label3
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Times New Roman", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label3.ForeColor = Color.Black;
-            label3.Location = new Point(12, 229);
+            label3.Location = new Point(41, 149);
             label3.Name = "label3";
             label3.Size = new Size(327, 36);
             label3.TabIndex = 7;
@@ -120,7 +118,7 @@
             label4.AutoSize = true;
             label4.Font = new Font("Times New Roman", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label4.ForeColor = Color.Black;
-            label4.Location = new Point(12, 265);
+            label4.Location = new Point(41, 185);
             label4.Name = "label4";
             label4.Size = new Size(264, 36);
             label4.TabIndex = 8;
@@ -131,7 +129,7 @@
             label5.AutoSize = true;
             label5.Font = new Font("Times New Roman", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label5.ForeColor = Color.Black;
-            label5.Location = new Point(12, 310);
+            label5.Location = new Point(41, 241);
             label5.Name = "label5";
             label5.Size = new Size(657, 17);
             label5.TabIndex = 7;
@@ -142,7 +140,7 @@
             label6.AutoSize = true;
             label6.Font = new Font("Times New Roman", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label6.ForeColor = Color.Black;
-            label6.Location = new Point(12, 327);
+            label6.Location = new Point(41, 258);
             label6.Name = "label6";
             label6.Size = new Size(692, 17);
             label6.TabIndex = 9;
@@ -153,35 +151,67 @@
             label7.AutoSize = true;
             label7.Font = new Font("Times New Roman", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label7.ForeColor = Color.Black;
-            label7.Location = new Point(12, 344);
+            label7.Location = new Point(41, 275);
             label7.Name = "label7";
             label7.Size = new Size(60, 17);
             label7.TabIndex = 10;
             label7.Text = "cupones.";
             // 
-            // Inicio
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Times New Roman", 27.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label8.ForeColor = Color.Black;
+            label8.Location = new Point(281, 345);
+            label8.Name = "label8";
+            label8.Size = new Size(260, 42);
+            label8.TabIndex = 11;
+            label8.Text = "Nueva colección";
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = GoldenBoots.Properties.Resources.zapatos;
+            pictureBox3.Location = new Point(98, 406);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(351, 331);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 7;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = GoldenBoots.Properties.Resources.tacones;
+            pictureBox4.Location = new Point(470, 406);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(211, 331);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 12;
+            pictureBox4.TabStop = false;
+            // 
+            // sobre_nosotros
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ButtonHighlight;
-            ClientSize = new Size(800, 749);
+            ClientSize = new Size(829, 749);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(label8);
             Controls.Add(label7);
             Controls.Add(label6);
             Controls.Add(label5);
             Controls.Add(label4);
             Controls.Add(label3);
-            Controls.Add(pictureBox3);
             Controls.Add(panel1);
-            Controls.Add(pictureBox1);
-            Controls.Add(pictureBox2);
             ForeColor = Color.White;
-            Name = "Inicio";
+            Name = "sobre_nosotros";
             Text = "pagina de inicio";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -190,7 +220,6 @@
 
         private PictureBox pictureBox1;
         private PictureBox pictureBox2;
-        private PictureBox pictureBox3;
         private Panel panel1;
         private Label label1;
         private Label label2;
@@ -199,5 +228,8 @@
         private Label label5;
         private Label label6;
         private Label label7;
+        private Label label8;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
     }
 }
