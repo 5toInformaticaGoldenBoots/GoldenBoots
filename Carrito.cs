68a8983d67b63b671d275a1c32ad363a758baa9f
Ian Pichardo <ian.pichardo@hotmail.com>
2025-01-15 22:33:41 -0400

    Carrito Vaiolet

--- Carrito.cs
+++ Carrito.cs
@@ -1,20 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GoldenBoots;
+using Microsoft.VisualBasic.ApplicationServices;
 using System.Windows.Forms;
+
+
 
 namespace GoldenBoots
 {
+
     public partial class Carrito : Form
     {
+
+        private int qone = 1;
+        private int qtwo = 1;
         public Carrito()
         {
             InitializeComponent();
+            cantityText.Text = Convert.ToString(qone);
+            cantityText2.Text = Convert.ToString(qtwo);
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            cantityText.Text = Convert.ToString(++qone);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            cantityText.Text = Convert.ToString(--qone);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            cantityText2.Text = Convert.ToString(++qtwo);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            cantityText2.Text = Convert.ToString(--qtwo);
+
+        }
+
+
     }
 }
