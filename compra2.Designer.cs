3215659b2674a8a95e276bce9f1c174e7d04388a
ALTF4Official <angeldavidriveraherrera025@gmail.com>
2025-02-05 08:03:31 -0400

    skibidisigmaponi
    ponmi skibidi chamba

--- compra2.Designer.cs
+++ compra2.Designer.cs
@@ -332,7 +332,7 @@
             label8.Font = new Font("Sitka Small", 24F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
             label8.Location = new Point(674, 178);
             label8.Name = "label8";
-            label8.Size = new Size(373, 48);
+            label8.Size = new Size(373, 47);
             label8.TabIndex = 63;
             label8.Text = "Sandalia para damas";
             // 
@@ -410,7 +410,7 @@
             label21.Font = new Font("Sitka Small", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label21.Location = new Point(654, 527);
             label21.Name = "label21";
-            label21.Size = new Size(63, 23);
+            label21.Size = new Size(63, 21);
             label21.TabIndex = 70;
             label21.Text = "Tallas:";
             // 
@@ -421,7 +421,7 @@
             label16.ForeColor = Color.DarkOrange;
             label16.Location = new Point(654, 417);
             label16.Name = "label16";
-            label16.Size = new Size(122, 31);
+            label16.Size = new Size(121, 30);
             label16.TabIndex = 69;
             label16.Text = "RD$1,448";
             // 
@@ -456,6 +456,7 @@
             button1.TabIndex = 75;
             button1.Text = "Añadir a carrito";
             button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
             // 
             // numericUpDown1
             // 
