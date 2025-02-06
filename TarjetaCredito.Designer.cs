9c464de14f9ffd6f5c2de2e9c5d1b90f522ec4a6
uia23 <Vaiolet2.0@icloud.com>
2025-02-06 09:53:30 -0400

    nana
    haha

--- TarjetaCredito.Designer.cs
+++ TarjetaCredito.Designer.cs
@@ -40,33 +40,40 @@
             pictureBox3 = new PictureBox();
             pictureBox4 = new PictureBox();
             label5 = new Label();
-            textBox4 = new TextBox();
             label6 = new Label();
             textBox5 = new TextBox();
             pictureBox5 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            label7 = new Label();
+            textBox4 = new TextBox();
+            label8 = new Label();
+            label9 = new Label();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(150, 115);
+            label1.Location = new Point(871, 219);
+            label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(134, 20);
+            label1.Size = new Size(206, 31);
             label1.TabIndex = 0;
             label1.Text = "Tarjetas de crédito";
             // 
             // pictureBox2
             // 
             pictureBox2.Image = Properties.Resources.Imagen_visa;
-            pictureBox2.Location = new Point(150, 152);
+            pictureBox2.Location = new Point(1002, 276);
+            pictureBox2.Margin = new Padding(4, 5, 4, 5);
             pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(63, 35);
+            pictureBox2.Size = new Size(75, 80);
             pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox2.TabIndex = 2;
             pictureBox2.TabStop = false;
@@ -74,9 +81,10 @@
             // pictureBox1
             // 
             pictureBox1.Image = Properties.Resources.mastercard2;
-            pictureBox1.Location = new Point(219, 138);
+            pictureBox1.Location = new Point(871, 276);
+            pictureBox1.Margin = new Padding(4, 5, 4, 5);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(75, 66);
+            pictureBox1.Size = new Size(75, 80);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 3;
             pictureBox1.TabStop = false;
@@ -84,64 +92,70 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label2.Location = new Point(150, 244);
+            label2.Font = new Font("Times New Roman", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(101, 247);
+            label2.Margin = new Padding(4, 0, 4, 0);
             label2.Name = "label2";
-            label2.Size = new Size(167, 25);
+            label2.Size = new Size(238, 34);
             label2.TabIndex = 4;
-            label2.Text = "Nombre del titular";
+            label2.Text = "Numero de tarjeta:";
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(150, 272);
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.Location = new Point(347, 243);
+            textBox1.Margin = new Padding(4, 5, 4, 5);
             textBox1.Multiline = true;
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(204, 28);
+            textBox1.Size = new Size(290, 44);
             textBox1.TabIndex = 5;
-            textBox1.Text = "       Como aparece en la tarjeta";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label3.Location = new Point(150, 314);
+            label3.Font = new Font("Times New Roman", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(87, 465);
+            label3.Margin = new Padding(4, 0, 4, 0);
             label3.Name = "label3";
-            label3.Size = new Size(180, 25);
+            label3.Size = new Size(260, 34);
             label3.TabIndex = 6;
-            label3.Text = "Fecha de expiración";
+            label3.Text = "Fecha de expiración:\r\n";
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(150, 342);
+            textBox2.BorderStyle = BorderStyle.FixedSingle;
+            textBox2.Location = new Point(130, 537);
+            textBox2.Margin = new Padding(4, 5, 4, 5);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(100, 23);
+            textBox2.Size = new Size(141, 31);
             textBox2.TabIndex = 7;
-            textBox2.Text = "Mes";
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(265, 342);
+            textBox3.Location = new Point(357, 537);
+            textBox3.Margin = new Padding(4, 5, 4, 5);
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(100, 23);
+            textBox3.Size = new Size(141, 31);
             textBox3.TabIndex = 8;
-            textBox3.Text = "Año";
             // 
             // label4
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label4.Location = new Point(433, 115);
+            label4.Location = new Point(1176, 219);
+            label4.Margin = new Padding(4, 0, 4, 0);
             label4.Name = "label4";
-            label4.Size = new Size(130, 20);
+            label4.Size = new Size(200, 31);
             label4.TabIndex = 9;
             label4.Text = "Tarjetas de débito";
             // 
             // pictureBox3
             // 
             pictureBox3.Image = Properties.Resources.banreservas__2_;
-            pictureBox3.Location = new Point(422, 138);
+            pictureBox3.Location = new Point(1161, 276);
+            pictureBox3.Margin = new Padding(4, 5, 4, 5);
             pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(75, 66);
+            pictureBox3.Size = new Size(77, 80);
             pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox3.TabIndex = 10;
             pictureBox3.TabStop = false;
@@ -149,9 +163,10 @@
             // pictureBox4
             // 
             pictureBox4.Image = Properties.Resources.bancopopular;
-            pictureBox4.Location = new Point(514, 138);
+            pictureBox4.Location = new Point(1298, 276);
+            pictureBox4.Margin = new Padding(4, 5, 4, 5);
             pictureBox4.Name = "pictureBox4";
-            pictureBox4.Size = new Size(75, 66);
+            pictureBox4.Size = new Size(78, 80);
             pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox4.TabIndex = 11;
             pictureBox4.TabStop = false;
@@ -159,58 +174,112 @@
             // label5
             // 
             label5.AutoSize = true;
-            label5.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label5.Location = new Point(422, 244);
+            label5.Font = new Font("Times New Roman", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(151, 354);
+            label5.Margin = new Padding(4, 0, 4, 0);
             label5.Name = "label5";
-            label5.Size = new Size(167, 25);
+            label5.Size = new Size(188, 34);
             label5.TabIndex = 12;
-            label5.Text = "Nombre del titular";
-            // 
-            // textBox4
-            // 
-            textBox4.Location = new Point(425, 272);
-            textBox4.Multiline = true;
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(204, 28);
-            textBox4.TabIndex = 13;
+            label5.Text = "Codigo Postal:";
+            label5.Click += label5_Click;
             // 
             // label6
             // 
             label6.AutoSize = true;
             label6.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label6.Location = new Point(422, 314);
+            label6.Location = new Point(871, 459);
+            label6.Margin = new Padding(4, 0, 4, 0);
             label6.Name = "label6";
-            label6.Size = new Size(188, 25);
+            label6.Size = new Size(284, 40);
             label6.TabIndex = 14;
             label6.Text = "Código de seguridad";
             // 
             // textBox5
             // 
-            textBox5.Location = new Point(425, 342);
+            textBox5.Location = new Point(858, 526);
+            textBox5.Margin = new Padding(4, 5, 4, 5);
+            textBox5.Multiline = true;
             textBox5.Name = "textBox5";
-            textBox5.Size = new Size(100, 23);
+            textBox5.Size = new Size(150, 42);
             textBox5.TabIndex = 15;
             textBox5.Text = "3 digitos";
             // 
             // pictureBox5
             // 
             pictureBox5.Image = Properties.Resources.pasos_codigo_tarjeta__2_;
-            pictureBox5.Location = new Point(531, 342);
+            pictureBox5.Location = new Point(1083, 526);
+            pictureBox5.Margin = new Padding(4, 5, 4, 5);
             pictureBox5.Name = "pictureBox5";
-            pictureBox5.Size = new Size(143, 33);
+            pictureBox5.Size = new Size(204, 42);
             pictureBox5.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox5.TabIndex = 16;
             pictureBox5.TabStop = false;
             // 
+            // pictureBox6
+            // 
+            pictureBox6.BackColor = SystemColors.ActiveCaptionText;
+            pictureBox6.Location = new Point(1, 0);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(1538, 137);
+            pictureBox6.TabIndex = 17;
+            pictureBox6.TabStop = false;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = SystemColors.ActiveCaptionText;
+            label7.Font = new Font("Times New Roman", 28F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.ForeColor = SystemColors.ButtonHighlight;
+            label7.Location = new Point(582, 45);
+            label7.Name = "label7";
+            label7.Size = new Size(426, 63);
+            label7.TabIndex = 18;
+            label7.Text = "Formas de pago ";
+            // 
+            // textBox4
+            // 
+            textBox4.BorderStyle = BorderStyle.FixedSingle;
+            textBox4.Location = new Point(347, 350);
+            textBox4.Margin = new Padding(4, 5, 4, 5);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(290, 44);
+            textBox4.TabIndex = 19;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Times New Roman", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.Location = new Point(68, 541);
+            label8.Name = "label8";
+            label8.Size = new Size(53, 23);
+            label8.TabIndex = 20;
+            label8.Text = "Mes:";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label9.Location = new Point(301, 541);
+            label9.Name = "label9";
+            label9.Size = new Size(54, 28);
+            label9.TabIndex = 21;
+            label9.Text = "Año:";
+            // 
             // TarjetaCredito
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = SystemColors.ButtonHighlight;
+            ClientSize = new Size(1650, 750);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(textBox4);
+            Controls.Add(label7);
+            Controls.Add(pictureBox6);
             Controls.Add(pictureBox5);
             Controls.Add(textBox5);
             Controls.Add(label6);
-            Controls.Add(textBox4);
             Controls.Add(label5);
             Controls.Add(pictureBox4);
             Controls.Add(pictureBox3);
@@ -223,13 +292,16 @@
             Controls.Add(pictureBox1);
             Controls.Add(pictureBox2);
             Controls.Add(label1);
+            Margin = new Padding(4, 5, 4, 5);
             Name = "TarjetaCredito";
             Text = "Form1";
+            Load += TarjetaCredito_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -248,9 +320,13 @@
         private PictureBox pictureBox3;
         private PictureBox pictureBox4;
         private Label label5;
-        private TextBox textBox4;
         private Label label6;
         private TextBox textBox5;
         private PictureBox pictureBox5;
+        private PictureBox pictureBox6;
+        private Label label7;
+        private TextBox textBox4;
+        private Label label8;
+        private Label label9;
     }
 }
--- TarjetaCredito.cs
+++ TarjetaCredito.cs
@@ -16,5 +16,15 @@ namespace GoldenBoots
         {
             InitializeComponent();
         }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TarjetaCredito_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
